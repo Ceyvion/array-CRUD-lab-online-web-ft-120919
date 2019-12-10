c14b85558da6b51d525ad11812dd1da645f8aff1
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-10 19:49:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,10 @@ def add_element_to_end_of_array(array, element)
   array << element
 end 
 
+def add_element_to_start_of_array(array, element)
+
+  array.unshift(element)
+end
 
 def using_include(array, element)
  array.include?(element)
