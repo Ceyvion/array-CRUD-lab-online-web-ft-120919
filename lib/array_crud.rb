ebad3df8f4f6c61fe7671e2d1fa3d4d9cc9213d2
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-10 19:41:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
-end
+   new_array = []
+end 
 
-def create_an_array
-  
-end
+def create_an_array 
+  new_array = ["Dog", "Cat", "Zebra", "Horse"]
+end 
 
 def add_element_to_end_of_array(array, element)
-  
+  new_array.push("Tiger", "Elephant")
+end 
+
+def using_include(array, element)
+ array.include?(element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def using_sort(array)
+ array.sort()
 end
 
-def remove_element_from_end_of_array(array)
-  
+def using_reverse(array)
+ array.reverse()
 end
 
-def remove_element_from_start_of_array(array)
-  
+def using_first(array)
+ array.first()
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def using_last(array)
+ array.last()
 end
 
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+def using_size(array)
+ array.size()
 end
